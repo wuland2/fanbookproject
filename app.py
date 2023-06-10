19beff99ac17ef5de3f93d2795ba672aa65ef418
Aini Wulandari <ainiw261@gmail.com>
2023-06-10 22:47:16 +0700

    My Commit

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://wuland:rotilapis@cluster0.hb9bawy.mongodb.net/?retryWrites=true&w=majority')
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-db = client.dbsparta
+MONGODB_URI =os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
